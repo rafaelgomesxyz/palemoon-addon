5f250554cd54466348df7fdb3d6b26730d64d66b
rafaelgssa <rafael.gssa@gmail.com>
2019-09-06 14:20:34 -0300

    Implement action

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,50 @@
 import * as core from '@actions/core';
 
+import axios from 'axios';
+import FormData from 'form-data';
+import fs from 'fs';
+
+interface IParams {
+  slug: string,
+  xpi: string,
+  username: string,
+  password: string
+}
+
+async function uploadAddon(params: IParams) {
+  const token = Buffer.from(`${params.username}:${params.password}`).toString('base64');
+
+  const data = new FormData();
+
+  data.append('slug', params.slug);
+  data.append('xpiUpload', fs.createReadStream(params.xpi));
+
+  const response = await axios.post(`https://addons.palemoon.org/panel/addons/?task=update&what=release&slug=${params.slug}`, data, {
+    headers: {
+      Authorization: `Basic ${token}`,
+      ...data.getHeaders()
+    }
+  });
+
+  if (response.data) {
+    const matches = response.data.match(/<h2>Unable\sto\sComply<\/h2><ul><li>(.+?)<\/li><ul>/);
+
+    if (matches) {
+      throw new Error(`${response.statusText}: ${matches[1]}`);
+    }
+  }
+}
+
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const slug = core.getInput('slug', { required: true });
+    const xpi = core.getInput('xpi', { required: true });
+    const username = core.getInput('username', { required: true });
+    const password = core.getInput('password', { required: true });
+    
+    await uploadAddon({ slug, xpi, username, password });
+
+    core.debug('Uploaded addon');
   } catch (error) {
     core.setFailed(error.message);
   }
